c1a7e1bb5b6ab059d8408b25e568a59e45f7a08a
vivek_mishra <vivek.mishra@gwynniebee.com>
2020-05-18 19:49:21 +0530

    added adapter

--- app/src/main/java/com/example/receiptreader/ui/HomeScreenFragment.kt
+++ app/src/main/java/com/example/receiptreader/ui/HomeScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.receiptreader.R
+import kotlinx.android.synthetic.main.fragment_home_screen.*
 
 class HomeScreenFragment : Fragment() {
 
@@ -22,6 +23,17 @@ class HomeScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        btn_add_bill.setOnClickListener {
+
+
+        }
+
+
+        btn_show_expenses.setOnClickListener {
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/receiptreader/viewmodel/ExpenseViewModel.kt
+++ app/src/main/java/com/example/receiptreader/viewmodel/ExpenseViewModel.kt
@@ -2,6 +2,16 @@ package com.example.receiptreader.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.receiptreader.data.ExpenseRepository
+import com.example.receiptreader.data.Filters
+import com.example.receiptreader.data.Item
+import java.util.ArrayList
 
 class ExpenseViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    fun getFilteredList(filters: Filters):ArrayList<Item> {
+       return ExpenseRepository.getExpenses(filters).value!!
+    }
+
 }
