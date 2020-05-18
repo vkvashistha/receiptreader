c4b8670a6ef9eefc0b637a11c24dec6a300666f7
Ayush Agrawal <ayush.agrawal@gwynniebee.com>
2020-05-19 00:22:43 +0530

    added view pager on stats screen.

--- app/src/main/java/com/example/receiptreader/ui/StatsScreenFragment.kt
+++ app/src/main/java/com/example/receiptreader/ui/StatsScreenFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.receiptreader.R
 import com.example.receiptreader.data.Filters
 import com.example.receiptreader.data.Item
@@ -15,21 +18,15 @@ import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.fragment_stats_screen.*
+import java.util.*
+
 
 class StatsScreenFragment : Fragment() {
 
-    private val statsViewModel: StatsViewModel by lazy {
-        getViewModelProvider(this, null).get(
-            StatsViewModel::class.java
-        )
-    }
-
-    private fun getViewModelProvider(
-        fragment: Fragment,
-        factory: ViewModelProvider.Factory?
-    ): ViewModelProvider {
-        return ViewModelProviders.of(fragment, factory)
-    }
+    private lateinit var statsViewModel: StatsViewModel
+    private lateinit var layouts: IntArray
+    private var mAdapter: ViewsSliderAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,59 +34,156 @@ class StatsScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(false)
+        statsViewModel = getViewModelProvider(this, null).get(StatsViewModel::class.java)
         return inflater.inflate(R.layout.fragment_stats_screen, container, false)
     }
 
+    fun getViewModelProvider(
+        fragment: Fragment,
+        factory: ViewModelProvider.Factory?
+    ): ViewModelProvider {
+        return ViewModelProviders.of(fragment, factory)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val filters = Filters("2020/05/10", "2020/05/15", "Grocery", "Big Bazaar")
-        val itemList: java.util.ArrayList<Item>
-        itemList = statsViewModel.getExpenses(filters)
-        setBarChart(view, itemList)
-        setPieChart(view, itemList)
+
+        val fromDate = et_from_date.text.toString()
+        val toDate = et_to_date.text.toString()
+        val category = et_category.text.toString()
+        val merchant = et_merchant.text.toString()
+
+        val itemList = statsViewModel.getExpenses(Filters(fromDate, toDate, category, merchant))
+        init(view, itemList)
     }
 
-    private fun setBarChart(
+    private fun init(
         view: View,
         itemList: ArrayList<Item>
     ) {
-        val barChart = view.findViewById<BarChart>(R.id.barchart)
-        val entries: ArrayList<BarEntry> = ArrayList()
-        for (i in 0 until itemList.size) {
-            entries.add(BarEntry(itemList[i].price.toFloat(), 0))
-        }
-        val bardataset = BarDataSet(entries, "Cells")
-        val labels = ArrayList<String>()
-        for (i in 0 until itemList.size){
-            labels.add(itemList[i].category.toString())
-        }
-        val data = BarData(labels, bardataset)
-        barChart.data = data // set the data and list of labels into chart
-        barChart.setDescription("Set Bar Chart Description Here") // set the description
-        bardataset.setColors(ColorTemplate.COLORFUL_COLORS)
-        barChart.animateY(5000)
+        layouts = intArrayOf(
+            R.layout.fragment_barchart,
+            R.layout.fragment_piechart
+        )
+        mAdapter = ViewsSliderAdapter(layouts, itemList)
+
+        view.findViewById<ViewPager2>(R.id.view_pager).adapter = mAdapter
+
+        view.findViewById<ViewPager2>(R.id.view_pager)
+            .registerOnPageChangeCallback(pageChangeCallback)
     }
 
-    private fun setPieChart(
-        view: View,
-        itemList: ArrayList<Item>
-    ) {
-        val piechart = view.findViewById<PieChart>(R.id.piechart)
-        val pieChartEntries: ArrayList<Entry> = ArrayList()
-        for (i in 0 until itemList.size) {
-            pieChartEntries.add(Entry(itemList[i].price.toFloat(), 0))
+    /*
+     * ViewPager page change listener
+     */
+    var pageChangeCallback: OnPageChangeCallback = object : OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
         }
-        val piedataset = PieDataSet(pieChartEntries, "Cells")
-        val label = ArrayList<String>()
-        for (i in 0 until itemList.size){
-            label.add(itemList[i].category.toString())
-        }
-        val piedata = PieData(label, piedataset)
-        piechart.data = piedata // set the data and list of labels into chart
-        piechart.setDescription("Set Bar Chart Description Here") // set the description
-        piedataset.setColors(ColorTemplate.COLORFUL_COLORS)
-        piechart.animateY(5000)
     }
 
+    open class ViewsSliderAdapter(
+        private var layouts: IntArray,
+        private var itemList: ArrayList<Item>
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(viewType, parent, false)
+            return SliderViewHolder(view)
+        }
+
+        override fun onBindViewHolder(
+            holder: RecyclerView.ViewHolder,
+            position: Int
+        ) {
+            if (position == 0) {
+                setBarChart(holder.itemView)
+            } else {
+                setPieChart(holder.itemView)
+            }
+        }
+
+        private fun setPieChart(
+            view: View
+        ) {
+            val hashMap = HashMap<String, Float>()
+            for (i in 0 until itemList.size) {
+                hashMap[itemList[i].category.toString()] = itemList[i].price.toFloat()
+            }
+
+            val piechart = view.findViewById<PieChart>(R.id.piechart)
+            val pieChartEntries: ArrayList<Entry> = ArrayList()
+            for (i in 0 until itemList.size) {
+                pieChartEntries.add(
+                    Entry(
+                        getExpenseForGivenCategory(
+                            itemList[i].category.toString(),
+                            hashMap
+                        ), 0
+                    )
+                )
+            }
+            val piedataset = PieDataSet(pieChartEntries, "Cells")
+            val label = ArrayList<String>()
+            for (i in 0 until itemList.size) {
+                label.add(itemList[i].category.toString())
+            }
+            val piedata = PieData(label, piedataset)
+            piechart?.data = piedata // set the data and list of labels into chart
+            piechart?.setDescription("Set Bar Chart Description Here") // set the description
+            piedataset.setColors(ColorTemplate.COLORFUL_COLORS)
+            piechart?.animateY(5000)
+        }
+
+        private fun setBarChart(itemView: View) {
+            val barChart = itemView.findViewById<BarChart>(R.id.barchart)
+            val hashMap = HashMap<String, Float>()
+            for (i in 0 until itemList.size) {
+                hashMap[itemList[i].category.toString()] = itemList[i].price.toFloat()
+            }
+            val entries: ArrayList<BarEntry> = ArrayList()
+            for (i in 0 until itemList.size) {
+                entries.add(
+                    BarEntry(
+                        getExpenseForGivenCategory(
+                            itemList[i].category.toString(),
+                            hashMap
+                        ), 0
+                    )
+                )
+            }
+            val bardataset = BarDataSet(entries, "Cells")
+            val labels = ArrayList<String>()
+            for (i in 0 until itemList.size) {
+                labels.add(itemList[i].category.toString())
+            }
+            val data = BarData(labels, bardataset)
+            barChart.data = data // set the data and list of labels into chart
+            barChart.setDescription("Set Bar Chart Description Here") // set the description
+            bardataset.setColors(ColorTemplate.COLORFUL_COLORS)
+            barChart.animateY(5000)
+        }
+
+        private fun getExpenseForGivenCategory(
+            category: String,
+            hashMap: HashMap<String, Float>
+        ): Float {
+            return hashMap[category]!!
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return layouts[position]
+        }
+
+        override fun getItemCount(): Int {
+            return layouts.size
+        }
+
+        inner class SliderViewHolder(view: View?) :
+            RecyclerView.ViewHolder(view!!) {
+        }
+    }
 }
